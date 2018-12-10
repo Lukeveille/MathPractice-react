854aef0b435eb7576ee0dbfa6608565388eac2b1
Lukeveille <luke@lukeleveille.com>
2018-12-09 19:36:17 -0500

    added favicon

--- App.js
+++ App.js
@@ -1,6 +1,30 @@
+function GameBox() {
+  return (
+    <div>
+      Highest integer you'd like to see:
+      <input id="range" value="12" type="text" class="fontStyle" size="5" />
+      How many questions?
+      <input id="questions" value="" type="text" class="fontStyle" size="5" />
+      <select id="mathType" class="fontStyle" type="select">
+        <option value="1">Addition/Subtraction</option>
+        <option value="2">Multiplication</option>
+        <option value="3">Division</option>
+      </select>
+      <input type="button" class="fontStyle" value="Let's do some math!" id="start" onmouseup="initialize()" />
+    </div>
+  )
+}
+
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <div>
+        <div class="game-box">
+          <GameBox />
+        </div>
+        <p id="msgBox" class="messages"></p>
+      </div>
+    )
   }
 }
 
